d04516b3ca6db92edde0fd66884906e4ae1911b6
Luiza Nunes <86207963+Luiza-Nunes@users.noreply.github.com>
2021-10-21 22:47:49 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const LinksSocialMedia = {
+  github: 'Luiza-Nunes',
+  youtube: '',
+  facebook: 'profile.php?id=100003534528774',
+  instagram: 'luiza_isabella',
+  twitter: ''
+}
+
+function chageSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+chageSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
